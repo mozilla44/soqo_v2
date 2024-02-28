7d38dbee6e55d2dac584cdecedc6c91a5111ae3d
Antoine <antoine.frisco@gmail.com>
2024-02-28 11:03:23 +0100

    last touches

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -18,7 +18,7 @@ export const Card = (props: BoxProps) => {
       borderRadius="10px"
       fontWeight="bold"
       transitionDuration="300ms"
-      boxShadow="0.2rem 0.2rem 1rem #173A2C"
+      boxShadow="0.2rem 0.2rem 1rem grey"
     >
       {children}
     </Box>
