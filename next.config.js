0aba6cf4fe48dd259fd46e7830a765e1a67bf65c
Antoine <antoine.frisco@gmail.com>
2024-11-07 19:11:00 +0100

    redirects

--- next.config.js
+++ next.config.js
@@ -1,6 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/projets/adidas-arena',  
+        destination: '/projets',     
+        permanent: true,                  
+      },
+      {
+        source: '/projets/des-etoiles-et-des-femmes-par-weavers-and-lavazza(.*)', // Regex match for variations
+        destination: '/projets',
+        permanent: true,
+      },
+      {
+        source: '/projets/autremonde-6eme_sens(.*)', // Regex match for variations
+        destination: '/projets',
+        permanent: true,
+      },
+     
+    ];
+  },
 }
 
-module.exports = nextConfig
+module.exports = nextConfig;
