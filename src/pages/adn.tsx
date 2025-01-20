ebe59624b3d28fedf1daf7f2c5d8c61c6284638f
Antoine <antoine.frisco@gmail.com>
2025-01-20 13:47:03 +0100

    fixed team picture ( no brands)

--- src/pages/adn.tsx
+++ src/pages/adn.tsx
@@ -298,7 +298,7 @@ const Adn: React.FC<AdnProps> = ({ logos, teamEntries }) => {
         bgColor={Color.BEIGE}
         textColor={Color.KAKI}
         mobileHeight={"30vh"}
-        imageSrc={"/assets/team2.jpg"}
+        imageSrc={"/assets/team3.jpg"}
         title={"Notre manifeste"}
         content={(
           <>
